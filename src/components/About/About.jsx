1ed141d5792ac47d3888d0e5c610543c4212ce6c
Saksham Gupta <sakshamprem2003@gmail.com>
2025-05-28 14:50:28 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactTypingEffect from "react-typing-effect";
 import Tilt from "react-parallax-tilt";
-import profileImage from "../../assets/mypic.jpg";
+import profileImage from "../../assets/user.jpg";
 
 const About = () => {
   return (
